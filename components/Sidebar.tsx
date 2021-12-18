b56a144e9f19f4d5e12ce1faae5450a39cfe2863
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2021-12-18 19:32:03 -0300

    adicionado next auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import { HeartIcon, HomeIcon, LibraryIcon, PlusCircleIcon, RssIcon, SearchIcon } from '@heroicons/react/outline';
+
 const Sidebar: React.FC = () => {
   return (
     <div className="text-gray-500 p-5 text-sm border-gray-900">
-      <div>
+      <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -18,7 +18,7 @@ const Sidebar: React.FC = () => {
           <p>Your Library</p>
         </button>
 
-        <hr className="border-t-[1px]" />
+        <hr className="border-t-[0.1px] border-gray-900" />
 
         <button className="flex items-center space-x-2 hover:text-white">
           <PlusCircleIcon className="h-5 w-5" />
@@ -32,6 +32,17 @@ const Sidebar: React.FC = () => {
           <RssIcon className="h-5 w-5" />
           <p>Your episodes</p>
         </button>
+
+        <hr className="border-t-[0.1px] border-gray-900" />
+
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Sidebar from 'components/Sidebar';
 export default function Home() {
   return (
     <div className="">
-      <main>
+      <main className="bg-black h-screen overflow-hidden">
         <Sidebar />
       </main>
 
