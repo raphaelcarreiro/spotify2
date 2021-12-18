49fcc92594a56fe0b6d18c4de786911c37385cae
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2021-12-18 18:23:21 -0300

    primeiro commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from 'components/Sidebar';
+
+export default function Home() {
+  return (
+    <div className="">
+      <main>
+        <Sidebar />
+      </main>
+
+      <div></div>
+    </div>
+  );
+}
